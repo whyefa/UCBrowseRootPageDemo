22acc20c51dfce1170f8140bcb0a735cbcd3d863
Broken <whyefa@163.com>
2015-12-30 11:03:52 +0800

    Initial Commit

---
+++ UCBrowseRootPageDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UCBrowseRootPageDemo
+//
+//  Created by Developer on 15/12/30.
+//  Copyright © 2015年 RC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ UCBrowseRootPageDemoTests/UCBrowseRootPageDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  UCBrowseRootPageDemoTests.swift
+//  UCBrowseRootPageDemoTests
+//
+//  Created by Developer on 15/12/30.
+//  Copyright © 2015年 RC. All rights reserved.
+//
+
+import XCTest
+@testable import UCBrowseRootPageDemo
+
+class UCBrowseRootPageDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
